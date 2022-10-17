c7d2bb56aecfe99a637a709dbfdfde527d57a314
test <test@KAB42-2>
2022-10-17 12:08:49 +0300

    Добавьте файлы проекта.

---
+++ Neg Rich Etkie/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите текс");
+string l = Console.ReadLine();
+char[] c = l.ToCharArray();
+int MaxCount = 0;
+int currentcount = 0;
+foreach (char c2 in c)
+{
+    if (char.IsDigit(c2))
+    {
+        currentcount++;
+        if (MaxCount <= currentcount)
+        {
+            MaxCount = currentcount;
+        }
+    }
+    else if (char.IsDigit(c2) == false)
+    {
+        currentcount = 0;
+        continue;
+    }                                                                                                    
+}
+Console.WriteLine($"максимальная последовательность чисел идущих подряд - {MaxCount}");
+
